2e7199276885d0e2690cf9e339d34c45e6cef15a
Zoe Piccirillo <zpiccirillo10@stuy.edu>
2021-01-14 06:21:46 +0000

    add functions shift_index and shift_letter to represent shifting of inner disk

--- main.py
+++ main.py
@@ -2,19 +2,52 @@ print("hello")
 import sys 
 import random
 
-lower_disk = {"a": 0, "b": 1, "c": 2, "d": 3, "e": 4, "f": 5, "g": 6, "h": 7, "i": 8, "j": 9, "k": 10, "l": 11, "m": 12, "n": 13, "o": 14, "p": 15, "q": 16, "r": 17, "s": 18, "t": 19, "u": 20, "v": 21, "w": 22, "x": 23, "y": 24, "z": 25} #reference disk to store letters and their indexes, will be used to match up shifted letters
+disk = {"A": 0, "B": 1, "C": 2, "D": 3, "E": 4, "F": 5, "G": 6, "H": 7, "I": 8, "J": 9, "K": 10, "L": 11, "M": 12, "N": 13, "O": 14, "P": 15, "Q": 16, "R": 17, "S": 18, "T": 19, "U": 20, "V": 21, "W": 22, "X": 23, "Y": 24, "Z": 25} #reference disk to store letters and their indexes, will be used to match up shifted letters
 
 #function to shift letters 
-  #input: start position uppercase letter, 
+  #input: start position uppercase letter
+def shift_index(upper_disk_key, lower_disk_key):
+  upper_disk_letters = list(disk.keys())
+  upper_index = disk[upper_disk_key] 
+  print(upper_index)
+  lower_index = disk[lower_disk_key] 
+  print(lower_index)
+  shift_index = upper_index - lower_index 
+  return shift_index
 
 #function to match up letters after shift 
-
+def shift_letter(shift_index, letter):
+  shifted_letter_i = disk[letter] + shift_index 
+  disk_letters = list(disk.keys())
+  if shifted_letter_i > 25: 
+    shifted_letter_i = shifted_letter_i % 26 
+  if shifted_letter_i < 0: 
+    shifted_letter_i = 26 + shifted_letter_i
+  shifted_letter = disk_letters[shifted_letter_i]
+  print(shifted_letter.lower())
+  
 #function to keep track of matching 
-  #add lowercase letter after each shift change (use random number from 0-25 to determine which letter will match up with the uppercase letter)
+  #add uppercase letter to indicate new shift change 
+  #determine new shift change with random number from 0-25
 
+#testing shift_letter() and shift_index()
+shift_index = shift_index("K", "S") 
+
+shift_letter(shift_index, "H")
+shift_letter(shift_index, "E")
+shift_letter(shift_index, "L")
+shift_letter(shift_index, "L")
+shift_letter(shift_index, "O")
+shift_letter(shift_index, "W")
+shift_letter(shift_index, "O")
+shift_letter(shift_index, "R")
+shift_letter(shift_index, "L")
+shift_letter(shift_index, "D")
+
+'''
 
 if __name__ == "__main__": 
 text = sys.argv[1]
 lower_disk_key = sys.argv[2]  #letter for start position (uppercase) (for the first shift, this will get matched with k because that's what Alberti did)
 shift_interval = sys.argv[3] #interval at which shift setting changes (every n letters)   
-
+'''
