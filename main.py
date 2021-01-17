a783cbf4756651ef3ad869a325e1ab9f9aaffdc3
Zoe Piccirillo <zpiccirillo10@stuy.edu>
2021-01-17 00:17:08 +0000

    refine decoding

--- main.py
+++ main.py
@@ -79,14 +79,14 @@ def decode_shift(outer_disk_key, inner_disk_key):
 #add uppercase letter to indicate new shift change
 #determine new shift change with random number from 0-25
 def encode(text, outer_disk_key, period_length):
-  print("Starting! \nYour text is: " + text + "\nThe outer disk key is: " + str(outer_disk_key) + "\n----------------")
+  print("Starting! \nYour plaintext is: " + text + "\nThe outer disk key is: " + outer_disk_key + "\n----------------")
     #text = text.replace(" ", "")  #delete all whitespace 
   cipher = ""
   num = 0 #position of letter to recognize when to shift
   for letter in text:
     if num == 0 or num == period_length: #the disk needs to be shifted
         num = 0
-        print("Beginning new shift. the cipher is currently: " + cipher)
+        print("Beginning new shift. The cipher is currently: " + cipher)
         inner_disk_key, shift_num = encode_shift(outer_disk_key) #begin new shift
         print("Encoding in progress...")
         cipher += inner_disk_key #start each shift with the key of the shift
@@ -96,18 +96,18 @@ def encode(text, outer_disk_key, period_length):
 
 #decode ciphertext
 def decode(text, outer_disk_key): 
-  print("The outer disk key is: " + outer_disk_key)
-  cipher = ""  
+  print("Starting! \nYour ciphertext is: " + text + "\nThe outer disk key is: " + outer_disk_key + "\n----------------")
+  plain_txt = ""  
   for letter in text: 
     if letter.isupper():
-      print(letter + " is the start of a shift.")
+      print(letter + " is the start of a shift. The plaintext is currently: " + plain_txt)
+      print("Decoding in progress...")
       shift_num = decode_shift(outer_disk_key, letter)
-      print("The shift index is " + str(shift_num))
       continue 
-    cipher += decode_letter(shift_num, letter.upper())
-  print(cipher)
+    plain_txt += decode_letter(shift_num, letter.upper())
+  print("--------------- \nFINAL PLAINTEXT: " + plain_txt)
     
-encode("zoepiccirillo", "A", 6)
+#encode("zoepiccirillo", "A", 6)
 decode("GfukvoiWyenehhOc", "A")
 
 
